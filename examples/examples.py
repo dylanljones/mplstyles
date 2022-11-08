87a716eb4f78b664554bb253f3922ea42c386268
dylanljones <dylanljones94@gmail.com>
2022-11-08 11:04:47 +0100

    fix: generate examples for all included styles

--- examples/examples.py
+++ examples/examples.py
@@ -6,15 +6,14 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from mplstyles import mplstyle_context
-
-STYLES = "figures.mplstyle", "plots.mplstyle"
+from mplstyles import get_mplstyles, mplstyle_context
 
 
 def main():
     x = np.linspace(-5, +5, 1001)
     y = np.sin(x)
-    for style in STYLES:
+
+    for style in get_mplstyles():
         with mplstyle_context(style):
             fig, ax = plt.subplots()
             ax.plot(x, y)
