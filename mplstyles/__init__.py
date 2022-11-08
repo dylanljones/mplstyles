0ef989b382816c6cd1522cb0caba6dc8a4a49200
dylanljones <dylanljones94@gmail.com>
2022-11-08 15:52:18 +0100

    fix: improve style name handling

--- mplstyles/__init__.py
+++ mplstyles/__init__.py
@@ -13,21 +13,34 @@ except ImportError:
     __version__ = "0.0.0"
 
 
-def use_mplstyle(name: str) -> None:
-    name = os.path.splitext(name)[0] + ".mplstyle"  # ensure the name ends with ext
-    file = os.path.join(os.path.dirname(__file__), name)
-    plt.style.use(file)
-
-
-def mplstyle_context(name: str):
-    name = os.path.splitext(name)[0] + ".mplstyle"  # ensure the name ends with ext
-    file = os.path.join(os.path.dirname(__file__), name)
-    return plt.style.context(file)
-
-
 def get_mplstyles():
     names = list()
     for name in os.listdir(os.path.dirname(__file__)):
         if os.path.splitext(name)[1] == ".mplstyle":
             names.append(name)
     return names
+
+
+def _get_style(name: str):
+    filename = name
+    ext = ".mplstyle"
+    if not filename.endswith(ext):
+        # ensure the name ends with ext
+        filename = filename + ext
+    file = os.path.join(os.path.dirname(__file__), filename)
+    if not os.path.exists(file):
+        styles = ", ".join(f"'{x}'" for x in get_mplstyles())
+        raise ValueError(
+            f"Input '{name}' is not a valid style! Supported styles are {styles}"
+        )
+    return file
+
+
+def use_mplstyle(name: str):
+    file = _get_style(name)
+    plt.style.use(file)
+
+
+def mplstyle_context(name: str):
+    file = _get_style(name)
+    return plt.style.context(file)
