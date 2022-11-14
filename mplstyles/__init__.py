a3fd7b79580078879db4cf2c2b179f8035872c96
dylanljones <dylanljones94@gmail.com>
2022-11-14 21:35:05 +0100

    fix: improve API and styles

--- mplstyles/__init__.py
+++ mplstyles/__init__.py
@@ -5,6 +5,7 @@
 # Copyright (c) 2022, Dylan Jones
 
 import os
+import pkg_resources
 import matplotlib.pyplot as plt
 
 try:
@@ -12,35 +13,48 @@ try:
 except ImportError:
     __version__ = "0.0.0"
 
+_initialized = False
+
 
 def get_mplstyles():
-    names = list()
-    for name in os.listdir(os.path.dirname(__file__)):
-        if os.path.splitext(name)[1] == ".mplstyle":
-            names.append(name)
-    return names
-
-
-def _get_style(name: str):
-    filename = name
     ext = ".mplstyle"
-    if not filename.endswith(ext):
-        # ensure the name ends with ext
-        filename = filename + ext
-    file = os.path.join(os.path.dirname(__file__), filename)
-    if not os.path.exists(file):
-        styles = ", ".join(f"'{x}'" for x in get_mplstyles())
-        raise ValueError(
-            f"Input '{name}' is not a valid style! Supported styles are {styles}"
-        )
-    return file
+    styles_dir = pkg_resources.resource_filename('mplstyles', "styles")
+    styles = list()
+    for root, _, files in os.walk(styles_dir):
+        for filename in files:
+            if filename.endswith(ext):
+                name = filename.removesuffix(ext)
+                styles.append(name)
+    return styles
 
 
-def use_mplstyle(name: str):
-    file = _get_style(name)
-    plt.style.use(file)
+def register_styles(styles_root_dir):
+    global _initialized
+
+    # Reads in defined styles
+    stylesheets = plt.style.core.read_style_directory(styles_root_dir)
+    for root, dirs, _ in os.walk(styles_root_dir):
+        for dirname in dirs:
+            path = os.path.join(root, dirname)
+            new_stylesheets = plt.style.core.read_style_directory(path)
+            stylesheets.update(new_stylesheets)
+
+    # Update dictionary of styles
+    plt.style.core.update_nested_dict(plt.style.library, stylesheets)
+    _initialized = True
 
 
-def mplstyle_context(name: str):
-    file = _get_style(name)
-    return plt.style.context(file)
+def init_mplstyles():
+    register_styles(pkg_resources.resource_filename('mplstyles', "styles"))
+
+
+def use_mplstyle(*name):
+    if not _initialized:
+        init_mplstyles()
+    plt.style.use(name)
+
+
+def mplstyle_context(*name):
+    if not _initialized:
+        init_mplstyles()
+    return plt.style.context(name)
