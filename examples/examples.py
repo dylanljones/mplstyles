3bca0705ee1ca3e1e259cc9e85c978ffd4ea9671
dylanljones <dylanljones94@gmail.com>
2022-11-14 21:37:10 +0100

    docs: update examples

--- examples/examples.py
+++ examples/examples.py
@@ -4,9 +4,12 @@
 #
 # Copyright (c) 2022, Dylan Jones
 
+import os.path
 import numpy as np
 import matplotlib.pyplot as plt
-from mplstyles import get_mplstyles, mplstyle_context
+from mplstyles import get_mplstyles, init_mplstyles
+
+init_mplstyles()
 
 
 def plot(x, y):
@@ -22,10 +25,27 @@ def main():
     x = np.linspace(-4 * np.pi, +4 * np.pi, 1000)
     y = np.sin(x) / x
 
-    for style in get_mplstyles():
-        with mplstyle_context(style):
+    style_list = [
+        ["plot"],
+        ["figure"],
+        ["plot", "aps"],
+        ["plot", "aps", "aps1.5"],
+        ["plot", "aps", "aps2"],
+        ["figure", "aps"],
+        ["figure", "aps", "aps1.5"],
+        ["figure", "aps", "aps2"],
+    ]
+
+    figdir = "figures"
+    if not os.path.exists(figdir):
+        os.makedirs(figdir)
+
+    for styles in style_list:
+        file = "_".join(styles) + ".png"
+        with plt.style.context(styles):
             fig, ax = plot(x, y)
-            fig.savefig(f"{style}.png")
+            print(f"Saving {file}")
+            fig.savefig(os.path.join(figdir, file))
 
 
 if __name__ == "__main__":
