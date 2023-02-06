703590c57e11f283d1b51c4c49b3382eb8177cbc
dylanljones <dylanljones94@gmail.com>
2023-02-06 16:24:55 +0100

    add method for showing plots in greyscale for testing color sequence

--- mplstyles/colors.py
+++ mplstyles/colors.py
@@ -8,6 +8,7 @@
 
 from typing import Sequence, Union
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 seaborn_colorblind = ["#0072B2", "#009E73", "#D55E00", "#CC79A7", "#F0E442", "#56B4E9"]
 tableau_colorblind = [
@@ -33,3 +34,16 @@ def set_colorcycle(color: Union[Sequence, str]) -> None:
     if isinstance(color, str):
         color = cycles[color]
     mpl.rcParams["axes.prop_cycle"] = mpl.cycler(color=color)
+
+
+def show_greyscale(file):
+    import numpy as np
+    from PIL import Image
+
+    image = Image.open(file).convert("L")
+
+    fig, ax = plt.subplots()
+    ax.axes.get_xaxis().set_visible(False)
+    ax.axes.get_yaxis().set_visible(False)
+    ax.imshow(np.asarray(image), cmap='gray', vmin=0, vmax=255)  # noqa
+    return fig, ax
